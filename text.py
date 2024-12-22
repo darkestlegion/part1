382d65b48b829eeca5e2e8adad165c49c70ba6d5
darkestlegion <145479089+darkestlegion@users.noreply.github.com>
2024-12-22 23:27:36 +0100

    gra w zgatywanie

--- text.py
+++ text.py
@@ -1,2 +1,13 @@
-print("world")
+odp = random.randint(1, 100)
+
+while (true) :
+    kostka = input("wspisz liczbę od 1 do 100")
+
+    if(kostka == odp): 
+        print("wygraeś")
+        break 
+    elif (kostka < opd):
+        print ("liczba jest więksksza")
+    else:
+        print ("liczba jest mniejsza")        
 
