988f9240eb3b86c19f7a29895044eb8825824c2c
darkestlegion <145479089+darkestlegion@users.noreply.github.com>
2024-12-24 09:08:02 +0100

    poprawy kółka i krzyzyk

--- kułko i krzyzyk.py
+++ kułko i krzyzyk.py
@@ -1,5 +1,15 @@
 import random
 
-lista_mózliwych_zagrań = [[1,1,1,0,0,0,0,0,0],[0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,1,1,1],[1,0,0,1,0,0,1,0,0],[0,1,0,0,1,0,0,1,0],[0,0,1,0,0,1,0,0,1],[1,0,0,0,1,0,0,0,1][0,0,1,0,1,0,1,0,0]]
+lista_mózliwych_zagrań_x = [[1,1,1,0,0,0,0,0,0],[0,0,0,1,1,1,0,0,0],[0,0,0,0,0,0,1,1,1],[1,0,0,1,0,0,1,0,0],[0,1,0,0,1,0,0,1,0],[0,0,1,0,0,1,0,0,1],[1,0,0,0,1,0,0,0,1][0,0,1,0,1,0,1,0,0]]
 
 plansza_gra = []
+
+tura = False
+
+while (True) : 
+    wybór_pola = int(input("wypisze pole"))
+    plansza_gra[wybór_pola] = tura 
+
+warunek = True
+warunek ? "wartość_jeśli_prawd"a : "wartość_jeśli_fałsz"
+
